adbeec1d80d97dc619414347483c65f962685baa
Juan Pablo Cardenas <juan.cardenas.r@usach.cl>
2023-05-30 23:48:35 -0400

    añadido el predicado de system

---
+++ TDAsystem_21209320_CardenasRueda.pl
@@ -0,0 +1,13 @@
+%Laboratorio 2 paradigmas: programación lógica
+
+
+
+%HECHOS
+
+%CLAUSULAS
+
+%Predicado de "system"
+%Constructor de sistema
+%Dominio: nombre del sistema X lista resultante añadiendo el nombre de sistema (sistema)
+%Una lista de 8 sublistas para almacenas: nombre, drives, usuarios, log, carpetas, archivos, papelera de carpetas, papelera de archivos.
+system(Nombre, [[Nombre],[],[],[],[],[],[],[]]).
