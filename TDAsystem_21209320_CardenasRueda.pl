6367acecfcecedfa35f4b0e9168277d47cdfb09a
Juan Pablo Cardenas <juan.cardenas.r@usach.cl>
2023-06-01 22:44:04 -0400

    añadido systemAddDrive

--- TDAsystem_21209320_CardenasRueda.pl
+++ TDAsystem_21209320_CardenasRueda.pl
@@ -4,10 +4,25 @@
 
 %HECHOS
 
+
 %CLAUSULAS
 
 %Predicado de "system"
 %Constructor de sistema
 %Dominio: nombre del sistema X lista resultante añadiendo el nombre de sistema (sistema)
-%Una lista de 8 sublistas para almacenas: nombre, drives, usuarios, log, carpetas, archivos, papelera de carpetas, papelera de archivos.
+%Meta: 1°: Entregar una lista de 8 para referenciar: nombre, drives, usuarios, log, carpetas, archivos, papelera de carpetas, papelera de archivos.
+
 system(Nombre, [[Nombre],[],[],[],[],[],[],[]]).
+
+%Predicado de "add-drive"
+%Constructor de drives
+%Dominio: Sistema X letra del drive X nombre del drive X capacidad del drive X sistema resultante
+%Meta:  1°:Añadir un drive de acuerdo a los datos entregados como letra, nombre y capacidad al sistema
+%	2°:Verificar si el drive ya se encuentra en el sistema.
+
+%Reglas (posiblemente se agregue al TDA a futuro)
+existeDrive(_, []):-fail. %Este caso es cuando NO existe el drive en sistema
+existeDrive(Letra, [[Letra|_]|_]). %En este caso, el drive ya existe (entrega "false")
+existeDrive(Letra,[_|Resto]):- existeDrive(Letra, Resto). %Recursivamente busca si el drive está en sistema.
+
+systemAddDrive([NombreSistema,Drives|Cola], Letra, Nombre, Capacidad, [NombreSistema,[[Letra,Nombre,Capacidad]|Drives]|Cola]):- \+ existeDrive(Letra, Drives).
